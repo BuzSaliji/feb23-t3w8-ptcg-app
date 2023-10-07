22a895dc509919ad8629cd51bd34f3421ed46121
Burim Saliji <buz.saliji@gmail.com>
2023-10-08 10:54:42 +1100

    fix some yellows

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import ApiProvider from './contexts/ApiContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import CardSearchByName from './Pages/CardSearchByName.jsx';
